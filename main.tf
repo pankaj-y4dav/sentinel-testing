981c235c76b8c93d3c059948fd739e4d7a221ddd
Pankaj Yadav <pankaj.yadav@hashicorp.com>
2025-08-12 18:50:23 +0530

    added ec2 resources with both encrypted and unencrypted root ebs

--- main.tf
+++ main.tf
@@ -18,6 +18,24 @@ provider "aws" {
   region = var.region
 }
 
+# EC2 Instance with encrypted root EBS volume
+resource "aws_instance" "test-server-encrypted" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 20
+    encrypted   = true
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = local.instance_name_encrypted
+    Type = "encrypted-root-volume"
+  }
+}
+
 # EC2 Instance with unencrypted root EBS volume
 resource "aws_instance" "test-server-unencrypted" {
   ami           = var.ami
