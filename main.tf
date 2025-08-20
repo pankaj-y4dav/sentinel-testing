ea1e0552eac28a447a838aafc8388fa9463e450d
Pankaj Yadav <pankaj.yadav@hashicorp.com>
2025-08-20 15:49:56 +0530

    added encrypted rds instance

--- main.tf
+++ main.tf
@@ -10,46 +10,60 @@ terraform {
 }
 
 locals {
-  instance_name_encrypted = "${terraform.workspace}-instance-encrypted"
-  instance_name_unencrypted = "${terraform.workspace}-instance-unencrypted"
+  db_name_encrypted = "${terraform.workspace}-db-encrypted"
+  db_name_unencrypted = "${terraform.workspace}-db-unencrypted"
 }
 
 provider "aws" {
   region = var.region
 }
 
-# EC2 Instance with encrypted root EBS volume
-resource "aws_instance" "test-server-encrypted" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  root_block_device {
-    volume_type = "gp3"
-    volume_size = 20
-    encrypted   = true
-    delete_on_termination = true
-  }
-
+# RDS Instance with encrypted storage
+resource "aws_db_instance" "test-db-encrypted" {
+  identifier = local.db_name_encrypted
+  
+  engine         = var.db_engine
+  engine_version = var.db_engine_version
+  instance_class = var.db_instance_class
+  
+  allocated_storage = var.db_allocated_storage
+  storage_type      = "gp3"
+  storage_encrypted = true
+  
+  db_name  = var.db_name
+  username = var.db_username
+  password = var.db_password
+  
+  skip_final_snapshot = true
+  deletion_protection = false
+  
   tags = {
-    Name = local.instance_name_encrypted
-    Type = "encrypted-root-volume"
+    Name = local.db_name_encrypted
+    Type = "encrypted-storage"
   }
 }
 
-# EC2 Instance with unencrypted root EBS volume
-resource "aws_instance" "test-server-unencrypted" {
-  ami           = var.ami
-  instance_type = var.instance_type
-
-  root_block_device {
-    volume_type = "gp3"
-    volume_size = 20
-    encrypted   = false
-    delete_on_termination = true
-  }
-
-  tags = {
-    Name = local.instance_name_unencrypted
-    Type = "unencrypted-root-volume"
-  }
-}
+# RDS Instance with unencrypted storage
+# resource "aws_db_instance" "test-db-unencrypted" {
+#   identifier = local.db_name_unencrypted
+  
+#   engine         = var.db_engine
+#   engine_version = var.db_engine_version
+#   instance_class = var.db_instance_class
+  
+#   allocated_storage = var.db_allocated_storage
+#   storage_type      = "gp3"
+#   storage_encrypted = false
+  
+#   db_name  = var.db_name
+#   username = var.db_username
+#   password = var.db_password
+  
+#   skip_final_snapshot = true
+#   deletion_protection = false
+  
+#   tags = {
+#     Name = local.db_name_unencrypted
+#     Type = "unencrypted-storage"
+#   }
+# }
--- variables.tf
+++ variables.tf
@@ -3,13 +3,57 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
 variable "ami" {
   description = "The AMI ID to use for the instance"
   type        = string
   default     = "ami-020cba7c55df1f615"
 }
+
 variable "instance_type" {
   description = "The type of instance to create"
   type        = string
   default     = "t2.micro"
+}
+
+variable "db_engine" {
+  description = "The database engine"
+  type        = string
+  default     = "mysql"
+}
+
+variable "db_engine_version" {
+  description = "The database engine version"
+  type        = string
+  default     = "8.0"
+}
+
+variable "db_instance_class" {
+  description = "The RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_allocated_storage" {
+  description = "The allocated storage in GB"
+  type        = number
+  default     = 20
+}
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+  default     = "testdb"
+}
+
+variable "db_username" {
+  description = "The master username for the database"
+  type        = string
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "The master password for the database"
+  type        = string
+  sensitive   = true
 }
